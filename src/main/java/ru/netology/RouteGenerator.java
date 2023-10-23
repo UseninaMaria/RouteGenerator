8cff62231e98331a66ddad7d310603c9b722dbbf
Usenina Maria <mariyausenina@gmail.com>
2023-10-23 17:24:57 +0400

    change method

--- src/main/java/ru/netology/RouteGenerator.java
+++ src/main/java/ru/netology/RouteGenerator.java
@@ -6,16 +6,35 @@ import java.util.Random;
 
 public class RouteGenerator {
     public static final Map<Integer, Integer> sizeToFreq = new HashMap<>();
+    private static final Object lock = new Object();
 
     public static void main(String[] args) {
 
         Thread[] threads = new Thread[1000];
+
+        Thread PrinterThread = new Thread(() -> {
+            while (!Thread.interrupted()) {
+                try {
+                    print();
+                    synchronized (lock) {
+                        lock.wait();
+                    }
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                }
+            }
+        });
+        PrinterThread.start();
 
         for (int i = 0; i < threads.length; i++) {
             threads[i] = new Thread(() -> {
                 String line = generateRoute("RLRFR", 100);
                 int rCount = countR(line);
                 updateSizeFreq(rCount);
+
+                synchronized (lock) {
+                    lock.notify();
+                }
             });
             threads[i].start();
         }
@@ -28,6 +47,7 @@ public class RouteGenerator {
             }
         }
 
+        PrinterThread.interrupt();
 
         int maxFreq = 0;
         for (int freq : sizeToFreq.values()) {
@@ -67,8 +87,26 @@ public class RouteGenerator {
         return count;
     }
 
-    public static synchronized void updateSizeFreq(int size) {
-        sizeToFreq.put(size, sizeToFreq.getOrDefault(size, 0) + 1);
+    public static void updateSizeFreq(int size) {
+        synchronized (sizeToFreq) {
+            sizeToFreq.put(size, sizeToFreq.getOrDefault(size, 0) + 1);
+        }
+    }
+
+    public static void print() {
+        synchronized (sizeToFreq) {
+            int maxFreq = 0;
+            int maxSize = 0;
+            for (Map.Entry<Integer, Integer> entry : sizeToFreq.entrySet()) {
+                int size = entry.getKey();
+                int freq = entry.getValue();
+                if (freq > maxFreq) {
+                    maxFreq = freq;
+                    maxSize = size;
+                }
+            }
+            System.out.println("Лидер " + maxSize + " . Частота: " + maxFreq + " раз)");
+        }
     }
 }
 
